002ffea6c41dcdb9697ab72f7f5d18c6cedcee3d
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2017-04-24 12:30:30 +0100

    Adds notes to remind to add Crystal System

--- uk.co.norphos.crystallography.toolkit/src/uk/co/norphos/crystallography/toolkit/Crystal.java
+++ uk.co.norphos.crystallography.toolkit/src/uk/co/norphos/crystallography/toolkit/Crystal.java
@@ -3,6 +3,7 @@ package uk.co.norphos.crystallography.toolkit;
 public class Crystal {
 	
 	private UnitCell unitCell;
+	private CrystalSystem crystalSystem;
 	
 	public Crystal(Lattice lattice) {
 		unitCell = new UnitCell(lattice);
