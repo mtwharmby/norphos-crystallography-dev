c50a1367ddf51a9e642b3fe819df4134cec0d3e8
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2017-04-25 01:19:42 +0100

    Completes implementation of CrystalSystem enum

--- uk.co.norphos.crystallography.toolkit/src/uk/co/norphos/crystallography/toolkit/CrystalSystem.java
+++ uk.co.norphos.crystallography.toolkit/src/uk/co/norphos/crystallography/toolkit/CrystalSystem.java
@@ -1,1 +1,7 @@
-public enum CrystalSystem
+package uk.co.norphos.crystallography.toolkit;
+
+public enum CrystalSystem {
+	TRICLINIC, MONOCLINIC, ORTHORHOMBIC, TETRAGONAL,
+	TRIGONAL, RHOMBOHEDRAL, HEXAGONAL, CUBIC; 
+
+}
