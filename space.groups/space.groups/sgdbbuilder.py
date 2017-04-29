da7faf43584597081850a5fbd0a5b33218709213
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2017-04-29 12:12:30 +0100

    Initial commit of space.groups python package

---
+++ space.groups/space.groups/sgdbbuilder.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+class DBBuilder(object):
+
+    def __init__(self):
+        #Read SQL DB schema file to make tables
+        #Get the cursor
+        pass
+
+    def insert_space_group(space_group):
+        #Given a space group object, insert its data into the tables
