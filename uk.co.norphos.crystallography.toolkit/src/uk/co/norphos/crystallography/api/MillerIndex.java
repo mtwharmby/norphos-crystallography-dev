0ce9b91c7dcc33a2748c95b1353bd3a2ac7434da
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2017-06-03 18:47:58 +0100

    Initial commit of MillerIndex class.
    Signed-off-by: Michael Wharmby <mtwharmby@gmail.com>

---
+++ uk.co.norphos.crystallography.toolkit/src/uk/co/norphos/crystallography/api/MillerIndex.java
@@ -0,0 +1,20 @@
+package uk.co.norphos.crystallography.api;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+
+public class MillerIndex extends Vector3D implements Comparable<MillerIndex> {
+
+	private static final long serialVersionUID = 1162662794043478530L;
+
+	public MillerIndex(double x, double y, double z) {
+		super(x, y, z);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int compareTo(MillerIndex arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
